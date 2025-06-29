105b611843150cc8b01b2da73f38a6207c58b0f7
bamaone251 <63915412+bamaone251@users.noreply.github.com>
2025-06-29 14:08:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 let messages = [];
 
